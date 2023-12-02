b9c0457ba5cc8efe71aa70c8bef76870cdcc6f62
Iftikhar uddin <iftipro@gmail.com>
2023-12-03 01:38:26 +0500

    Deploy script ready and token also done

--- web3/scripts/deploy.js
+++ web3/scripts/deploy.js
@@ -1,11 +1,21 @@
 const hre = require("hardhat");
 
 async function main() {
+  // Staking
   const tokenStaking = await hre.ethers.deployContract("TokenStaking");
 
   await tokenStaking.waitForDeployment();
 
-  console.log(` STACKING: ${tokenStaking.target}`);
+  console.log(` Staking: ${tokenStaking.target}`);
+
+  //TOKEN
+  const theblockchaincoders = await hre.ethers.deployContract(
+      "Theblockchaincoders"
+  );
+
+  await theblockchaincoders.waitForDeployment();
+
+  console.log(` Theblockchaincoders: ${theblockchaincoders.target}`);
 }
 
 main().catch((error) => {
